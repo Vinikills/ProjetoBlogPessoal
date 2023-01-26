0b88ac5f8487bde2672f9eda21914c0de7ac848d
Vinicius Fernando <viniferan@gmail.com>
2023-01-26 00:43:20 -0300

    ativ

---
+++ repository/PostagemRepository.java
@@ -0,0 +1,12 @@
+package org.generation.blogpessoal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import org.generation.blogpessoal.model.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+
+}
